8450561ea0e7199144dc21020ad43364ceb321d8
Roghayeh Heidari <roghayeh.heidari@ucalgary.ca>
2022-07-13 18:28:34 -0600

    Last Version Reset

--- ReadBinary.cpp
+++ ReadBinary.cpp
@@ -2,29 +2,42 @@
 // A small tool for reading binary files
 
 #include <iostream>
-
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
-#include <string>
-#include <stdio.h>
-#include <stdlib.h>
-#include <vector>
-
+#include <fstream>
 using namespace std;
 
-int main() {
-    std::cout << "Hello, STB_Image" << std::endl;
+int main()
+{
+	//you can set the file path here c:\\test\\myfile.t" 
+	const char* infilename = "myfile.x";
+	int size = 0;
+	short* indata;
+	int i = 0;
+	short tmp = 0;
 
-    string inputPath = "2017.jpg";
-    int iw, ih, n;
 
-    // Loading Pictures to Get Wide, High and Color Channel Information
-    unsigned char* idata = stbi_load(inputPath.c_str(), &iw, &ih, &n, 0);
-    
-    for(int i=0;i<50;i++)
-        cout << (int)idata[i] << '\t' ;
+	ifstream inf(infilename, std::ifstream::in | std::ifstream::binary);
+	if (inf.good())
+	{
+		inf.seekg(0, std::ifstream::end);
+		size = (int)inf.tellg() / sizeof(short);
+	}
+	cout << "this file has " << size << " values in it.\n Enter any key to see the values!\n";
+	//added just for pause 
+	getchar();
+	indata = new short[size];
+	inf.seekg(0);
+	while (inf.good() && !inf.eof() && i < size)
+	{
+		//if your file has double / int or other types just change short to that one 
+		//you have to chage line 12 (the array definition) and line 25 for the new statement and line 22 for calculation of number of items in the file
 
-    stbi_image_free(idata);
-    
-    return 0;
+		inf.read((char*)&tmp, sizeof(short));
+		indata[i++] = tmp;
+		cout << tmp << "\t";
+	}
+	inf.close();
+	cout << "\n\n\nI have read " << i << " values; " << "\n End of the reading process.\nPress any key to exit!\n\n\n";
+	//added just for pause 
+	getchar();
+	return 0;
 }
